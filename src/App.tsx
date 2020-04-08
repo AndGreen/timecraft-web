689e4d057593300f0c66d92c7a9ac35a14c171e5
zelenin_andrew <zelenin.andrew@gmail.com>
2020-04-09 00:31:05 +0300

    base template

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,21 @@ const Page = styled.div`
   margin: 0 auto;
 `;
 
+const PageHead = styled.div`
+  padding-top: 73px;
+`;
+const PageTitle = styled.div`
+  padding-bottom: 27px;
+`;
+const PageActions = styled.div`
+  padding-bottom: 24px;
+`;
+const PageContent = styled.div``;
+
 const Logo = styled.div`
   font-size: ${theme.sizes.title};
   color: ${theme.colors.main};
   font-weight: bold;
-  margin-top: 73px;
-  margin-bottom: 27px;
 `;
 
 type ColorPickerProps = {
@@ -67,12 +76,77 @@ const ColorPicker = styled.div<ColorPickerProps>`
   }
 `;
 
+const DayGrid = styled.div`
+  display: table;
+  border-collapse: collapse;
+  margin-left: 12px;
+`;
+const Line = styled.div`
+  display: table-row;
+`;
+const Label = styled.div`
+  display: table-cell;
+  padding-right: 12px;
+  font-size: ${theme.sizes.small};
+  color: ${theme.colors.main};
+  vertical-align: middle;
+`;
+const DayBlock = styled.div`
+  width: 35px;
+  height: 35px;
+  display: table-cell;
+  border: 1px solid ${theme.colors.borders};
+  &:nth-child(4),
+  &:nth-child(7) {
+    border-right-width: 3px;
+  }
+`;
+
+const numOnLines = 8;
+const numOfBlocksInLine = 9;
+const linesLabels = [
+  '00:00',
+  '03:00',
+  '06:00',
+  '09:00',
+  '12:00',
+  '15:00',
+  '18:00',
+  '21:00',
+];
+
+let days: string[][] = [];
+for (let i = 0; i < numOnLines; i++) {
+  days[i] = [];
+  for (let j = 0; j < numOfBlocksInLine; j++) {
+    days[i][j] = '';
+  }
+}
+
 function App() {
   return (
     <>
       <Page>
-        <Logo>Smoothy</Logo>
-        <ColorPicker>Color</ColorPicker>
+        <PageHead>
+          <PageTitle>
+            <Logo>Smoothy</Logo>
+          </PageTitle>
+          <PageActions>
+            <ColorPicker>Color</ColorPicker>
+          </PageActions>
+        </PageHead>
+        <PageContent>
+          <DayGrid>
+            {days.map((line, lineNum) => (
+              <Line key={`day-line-${lineNum}`}>
+                <Label>{linesLabels[lineNum]}</Label>
+                {line.map((day, i) => (
+                  <DayBlock key={`day-block-${lineNum}-${i}`} />
+                ))}
+              </Line>
+            ))}
+          </DayGrid>
+        </PageContent>
       </Page>
       <Styles />
     </>
