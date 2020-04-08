c207920ae9dd7bb724b8fe0c414f4518f14aa1db
zelenin_andrew <zelenin.andrew@gmail.com>
2020-04-08 21:59:55 +0300

    added title

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import { GlobalStyles } from './GlobalStyles';
-import './App.css';
+import styled from 'styled-components';
+import { Styles, theme } from './styles';
+
+const Page = styled.div`
+  width: 390px;
+  margin: 0 auto;
+`;
+const Logo = styled.div`
+  font-size: ${theme.sizes.title};
+  color: ${theme.colors.main};
+  font-weight: bold;
+  margin-top: 73px;
+`;
 
 function App() {
   return (
     <>
-      <GlobalStyles />
+      <Page>
+        <Logo>Smoothy</Logo>
+      </Page>
+      <Styles />
     </>
   );
 }
