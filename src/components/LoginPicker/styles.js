b9e6a2544dc3fcba8fd81429dd718263f3ee8a25
zelenin_andrew <zelenin.andrew@gmail.com>
2020-05-12 13:13:03 +0300

    sign in styles

--- src/components/LoginPicker/styles.js
+++ src/components/LoginPicker/styles.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import { theme } from '../../styles';
-import { arrowDownCss, arrowUpCss } from '../Icons/arrow';
 
 export const Wrapper = styled.div`
   position: relative;
@@ -18,6 +17,71 @@ export const Popup = styled.div`
   width: 390px;
   height: 500px;
   background: ${theme.colors.background};
+  .firebaseui-card-header {
+    padding: 10px 24px 0;
+  }
+  .firebaseui-container {
+    background: ${theme.colors.background};
+    box-shadow: none;
+  }
+  .firebaseui-id-idp-button,
+  .firebaseui-idp-text-long {
+    font-family: 'Roboto Mono';
+    font-size: 13px;
+  }
+  .firebaseui-title {
+    opacity: 0.4;
+  }
+  .firebaseui-subtitle {
+    font-size: 16px;
+  }
+  .firebaseui-text {
+    font-size: 14px;
+  }
+  .firebaseui-title,
+  .firebaseui-label,
+  .firebaseui-input,
+  .firebaseui-subtitle,
+  .firebaseui-text,
+  .firebaseui-input-invalid {
+    font-family: 'Roboto Mono';
+    color: ${theme.colors.font};
+  }
+  .firebaseui-id-secondary-link {
+    font-family: 'Roboto Mono';
+    color: ${theme.colors.font} !important;
+  }
+  .mdl-button--raised.mdl-button--colored,
+  .mdl-button.mdl-button--colored {
+    font-family: 'Roboto Mono';
+    background: ${theme.colors.main};
+    box-shadow: none;
+    color: black;
+  }
+  .mdl-button--raised.mdl-button--colored:active,
+  .mdl-button--raised.mdl-button--colored:focus:not(:active),
+  .mdl-button--raised.mdl-button--colored:hover {
+    background: ${theme.colors.main};
+    opacity: 0.8;
+  }
+  .firebaseui-textfield.mdl-textfield .firebaseui-label:after {
+    background-color: ${theme.colors.main};
+  }
+  .firebaseui-textfield.mdl-textfield .firebaseui-input {
+    border-color: ${theme.colors.font};
+  }
+  .mdl-progress.mdl-progress--indeterminate > .bar1,
+  .mdl-progress.mdl-progress__indeterminate > .bar1 {
+    background-color: ${theme.colors.main};
+  }
+  .bufferbar,
+  .auxbar {
+    background-image: none !important;
+    background-color: initial !important;
+  }
+  .mdl-progress.firebaseui-busy-indicator {
+    top: 40px;
+  }
 `;
 
 export const Title = styled.div`
